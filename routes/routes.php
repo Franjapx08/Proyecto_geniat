96cf47bc4c1ee8720bc0af59f8f457fb6c054322
Francisco Avalos <fjavpra@gmail.com>
2022-09-18 03:09:10 -0600

    validar rutas aplicando permisos usuario

--- routes/routes.php
+++ routes/routes.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'Helpers/ResponseJson.php'; 
 require_once 'Helpers/AuthValidation.php';
+require_once 'Helpers/PermisosValidation.php';
 require_once 'config/ConfigPut.php';
 
 /* obtener nombre de la url despues de / */
@@ -8,7 +9,8 @@ $routes_array = explode("/", $_SERVER['REQUEST_URI']);
 /* eliminar valores vacios */
 $routes_array = array_filter($routes_array);
 /* invocar helpers */
-$auth_validation = new AuthValidation(); 
+$auth_validation = new AuthValidation();
+$permisos_validation = new PermisosValidation();
 $responseJson = new ResponseJson();
 /* si no se encuentra una ruta */
 if(count($routes_array) == 0){
@@ -16,8 +18,6 @@ if(count($routes_array) == 0){
 }
 /* validar que la url contenga uno o maximo dos parametros y se use un metodo http */
 if(count($routes_array) == 1 || count($routes_array) == 2 && isset($_SERVER['REQUEST_METHOD'])){
-/*     echo 'aca';
-    return; */
     /* Registro de usuario */
     if($_SERVER['REQUEST_METHOD'] == 'POST' && $routes_array[1] == "registro"){
         require_once 'Controllers/UsuariosController.php';
@@ -31,52 +31,90 @@ if(count($routes_array) == 1 || count($routes_array) == 2 && isset($_SERVER['REQ
         require_once 'Controllers/UsuariosController.php';
         $usuario = new UsuariosController();
         $response = $usuario->login($_POST['correo'], $_POST['password']);
+        /* Comprobar permiso tipo consulta */
+        if(!$permisos_validation::comprobarPermisos($response, 'acceso')){
+            /* ruta bloqueada por permisos */
+            $response = $responseJson::error("No cuenta con los permisos necesarios para acceder", 403);
+            echo json_encode($response);
+            return;
+        }
         echo json_encode($response);
         return;
     }
     /* validar token */
-  /*   echo $auth_validation::validate();
-    return; */
-    if($auth_validation::validate()){
+    $token = $auth_validation::validate();
+    if($token){
         /* Obtener Publicaciones */
         if($_SERVER['REQUEST_METHOD'] == 'GET' && $routes_array[1] == "publicaciones"){
-            require_once 'Controllers/PublicacionesController.php';
-            $publicaciones = new PublicacionesController();
-            $response = $publicaciones->getPublicaciones();
-            echo json_encode($response);
-            return;
+            /* Comprobar permiso tipo consulta */
+            if($permisos_validation::comprobarPermisos($token, 'consulta')){
+                require_once 'Controllers/PublicacionesController.php';
+                $publicaciones = new PublicacionesController();
+                $response = $publicaciones->getPublicaciones();
+                echo json_encode($response);
+                return;
+            }else{
+                /* ruta bloqueada por permisos */
+                $response = $responseJson::error("No cuenta con los permisos necesarios para acceder", 403);
+                echo json_encode($response);
+                return;
+            }
         }
         /* Crear Publicaciones */
         if($_SERVER['REQUEST_METHOD'] == 'POST' && $routes_array[1] == "publicaciones"){
-            require_once 'Controllers/PublicacionesController.php';
-            $publicaciones = new PublicacionesController();
-            $response = $publicaciones->createPublicacion($_POST['titulo'], $_POST['descripcion'], $auth_validation::validate());
-            echo json_encode($response);
-            return;
+            /* Comprobar permiso tipo agregar */
+            if($permisos_validation::comprobarPermisos($token, 'agregar')){
+                require_once 'Controllers/PublicacionesController.php';
+                $publicaciones = new PublicacionesController();
+                $response = $publicaciones->createPublicacion($_POST['titulo'], $_POST['descripcion'], $token);
+                echo json_encode($response);
+                return;
+            }else{
+                /* ruta bloqueada por permisos */
+                $response = $responseJson::error("No cuenta con los permisos necesarios para acceder", 403);
+                echo json_encode($response);
+                return;
+            }
         }
         /* Editar Publicaciones */
         if($_SERVER['REQUEST_METHOD'] == 'PUT' && $routes_array[1] == "publicaciones" && $routes_array[2]){
-            require_once 'Controllers/PublicacionesController.php';
-            $config_put = new ConfigPut();
-            $_PUT = $config_put::getDataFromPut(json_encode(file_get_contents("php://input")));
-            $usuario = new PublicacionesController();
-            $response = $usuario->updatePublicacion($_PUT['titulo'], $_PUT['descripcion'], $routes_array[2]);
-            echo json_encode($response);
-            return;
+            /* Comprobar permiso tipo actualizar */
+            if($permisos_validation::comprobarPermisos($token, 'actualizar')){
+                require_once 'Controllers/PublicacionesController.php';
+                $config_put = new ConfigPut();
+                $_PUT = $config_put::getDataFromPut(json_encode(file_get_contents("php://input")));
+                $usuario = new PublicacionesController();
+                $response = $usuario->updatePublicacion($_PUT['titulo'], $_PUT['descripcion'], $routes_array[2]);
+                echo json_encode($response);
+                return;
+            }else{
+                /* ruta bloqueada por permisos */
+                $response = $responseJson::error("No cuenta con los permisos necesarios para acceder", 403);
+                echo json_encode($response);
+                return;
+            }
         }
         /* Eliminar Publicaciones */
         if($_SERVER['REQUEST_METHOD'] == 'DELETE' && $routes_array[1] == "publicaciones" && $routes_array[2]){
-            require_once 'Controllers/PublicacionesController.php';
-            $publicaciones = new PublicacionesController();
-            $response = $publicaciones->deletePublicacion($routes_array[2]);
-            echo json_encode($response);
-            return;
+            /* Comprobar permiso tipo eliminar */
+            if($permisos_validation::comprobarPermisos($token, 'eliminar')){
+                require_once 'Controllers/PublicacionesController.php';
+                $publicaciones = new PublicacionesController();
+                $response = $publicaciones->deletePublicacion($routes_array[2]);
+                echo json_encode($response);
+                return;
+            }else{
+                /* ruta bloqueada por permisos */
+                $response = $responseJson::error("No cuenta con los permisos necesarios para acceder", 403);
+                echo json_encode($response);
+                return;
+            }
         }
         /* Logout */
         if($_SERVER['REQUEST_METHOD'] == 'GET' && $routes_array[1] == "logout"){
             require_once 'Controllers/UsuariosController.php';
             $usuario = new UsuariosController();
-            $response = $usuario->logout($auth_validation::validate());
+            $response = $usuario->logout($token);
             echo json_encode($response);
             return;
         }
