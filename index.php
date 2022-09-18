8a9999978f788b852b579e1dba9562e9fb829461
Francisco Avalos <fjavpra@gmail.com>
2022-09-18 02:33:59 -0600

    index inicial, carga de router

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
- <!DOCTYPE html>  
-     <head>  
-      <title>Hello World!</title>
-     </head>  
+<?php
 
-     <body>  
-      <h1>Hello World!</h1>
-      <p><?php echo 'We are running PHP, version: ' . phpversion(); ?></p>
-     </body>
+/* errores */
+ini_set('desiplay_errors', 1);
+ini_set('log_errors', 1);
+//ini_set('error_log', ruta local);
+
+require_once "Controllers/RoutesController.php";
+
+$index = new RoutesController();
+$index->index();
