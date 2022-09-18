7f6a67b8e4baf38621d24af4547c9b975ad7f55f
Francisco Avalos <fjavpra@gmail.com>
2022-09-18 03:08:23 -0600

    validar permisos usuario

---
+++ Helpers/PermisosValidation.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once 'Models/Autenticacion.php';
+require_once 'Models/Permisos.php';
+require_once 'Models/Usuarios.php';
+
+class PermisosValidation{
+
+    public static function comprobarPermisos($token, $accion)
+    {
+        if($token){
+            /* decode token */
+            $modelo_auth = new Autenticacion();
+            $token_decode = $modelo_auth->jwtDecode($token);
+            /* obtener usuario para validar token */
+            $modelo_usuarios = new Usuarios();
+            $usuario = $modelo_usuarios->getUsuario($token_decode->data->correo);
+            /* obtener permisos */
+            $modelo_permisos = new Permisos();
+            $permisos = $modelo_permisos->getPermisos($usuario[0]['id']);
+            foreach($permisos as $permiso){
+                if(in_array($accion, $permiso)){
+                    return true;
+                }
+            }
+            return false;        
+        }
+        return false;
+    }
+}
